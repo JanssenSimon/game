ad2ad949ed044be4c5ca5baaac7c1c8deed9aa97
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-11-01 16:33:36 -0500

    Finishes structure for serverside networking

--- server.lua
+++ server.lua
@@ -1,13 +1,25 @@
 socket = require "socket"
-udp = socket.udp()
 
+otherCharacters = {}
+
+udp = socket.udp()
 udp:settimeout(0)
-udp:setsockname('*', 6969)
+udp:setsockname('*', 25565)
 
 running = true
 
+print "Beginning server loop."
 while running do
-    running = false
-end
+    data, msg_or_ip, port_or_nil = udp:receivefrom()
 
-print("server shutdown")
+    if data then
+        --parse data with data:match()
+    elseif msg_or_ip ~= 'timeout' then
+		--error("Unknown network error: "..tostring(msg))
+	end
+ 
+	socket.sleep(0.01)
+
+    running = false
+    print("Server shutdown.")
+end
