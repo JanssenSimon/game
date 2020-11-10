5e8b88ede8bf7120a5a9fd50616e89e44a30db13
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-11-10 11:20:17 -0500

    Server sends code more code more equitably to clients

--- server.lua
+++ server.lua
@@ -10,22 +10,27 @@ running = true
 
 print "Beginning server loop."
 while running do
-    data, msg_or_ip, port_or_nil = udp:receivefrom()
+    dat, msg_or_ip, port_or_nil = udp:receivefrom()
 
-    if data then
-        id = string.sub(data, 1, string.find(data, " ")-1)
-        characters[id] = data
-        print("Data received from "..id.." : "..data)
-
-        for c, dat in pairs(characters) do
-            if c ~= id then
-                print("Sending "..dat.." to "..id.." at "..msg_or_ip..":"..port_or_nil)
-                udp:sendto(dat, msg_or_ip, port_or_nil)
-            end
-        end
+    if dat then
+        id = string.sub(dat, 1, string.find(dat, " ")-1)
+        --print("Data received from "..id.." : "..dat)
+        characters[id] = {}
+        characters[id].data = dat
+        characters[id].ip = msg_or_ip
+        characters[id].port = port_or_nil
     elseif msg_or_ip ~= 'timeout' then
 		error("Unknown network error: "..tostring(msg))
 	end
+
+    for id1, c1 in pairs(characters) do 
+        for id2, c2 in pairs(characters) do
+            if id2 ~= id1  then
+                --print("Sending "..c2.data.." to "..id1.." at "..c1.ip..":"..c1.port)
+                udp:sendto(c2.data, c1.ip, c1.port)
+            end
+        end
+    end
  
 	socket.sleep(0.01)
 
