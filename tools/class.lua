e3ddea2bdf418bf10899441f8b31551333510ab2
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-11-22 23:22:38 -0500

    Instantiate class values that are arrays by value instead of by index

--- tools/class.lua
+++ tools/class.lua
@@ -9,8 +9,13 @@ function class.makeFrom(primitives)
 
     for k1, v1 in pairs(primitives) do
         for k2, v2 in pairs(v1) do
-            --TODO if the value is a table, copy it by value and not by index
             if not newClass[k2] then
+                -- if the value is a table, copy it by value and not by index
+                if type(v2) == "table" then
+                    for k3,v3 in pairs(v2) do
+                        newClass[k2][k3] = v3
+                    end
+                end
                 newClass[k2] = v2
             else
                 --TODO if both things are functions, return a megafunction that contains the whole
