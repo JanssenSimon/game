c91fc2db9d65ecb4de58b22dd9eebe18aaead3e8
LordFarquadTheGreat <moniquetamere1@gmail.com>
2020-11-25 14:58:32 -0500

    Allows for different characters to have different armors

--- tools/class.lua
+++ tools/class.lua
@@ -11,14 +11,17 @@ function class.makeFrom(primitives)
         for k2, v2 in pairs(v1) do
             if not newClass[k2] then
                 -- if the value is a table, copy it by value and not by index
+                -- TODO this but recursively
                 if type(v2) == "table" then
+                    newClass[k2] = {}
                     for k3,v3 in pairs(v2) do
                         newClass[k2][k3] = v3
                     end
+                else
+                    newClass[k2] = v2
                 end
-                newClass[k2] = v2
             else
-                --TODO if both things are functions, return a megafunction that contains the whole
+                --TODO if both things are functions, return a megafunction that contains everything
             end
         end
     end
